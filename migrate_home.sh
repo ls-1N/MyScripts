669aea3fcd378301b20876f661fbc8714ab5843d
i-need-to-tell-you-something <toomassoomaa+github@gmail.com>
2016-08-17 22:39:59 +0300

    Add .gitignore. Make script location a variable. Edit help message.

--- migrate_home.sh
+++ migrate_home.sh
@@ -2,20 +2,21 @@
 
 src="$1";
 dest="$2";
-#the path of the file where the lines of filenames to be copied are
-input="$HOME/Scripts/migrate_home_paths"
+#The path of this script.
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #The message that gets displayed as help
 help_message="Usage: migrate_home.sh SOURCE DEST
-Copy from SOURCE to DEST all the files and directories (including all contained subdirectories and files) mentioned in $input.
+Copy from SOURCE to DEST all the files and directories (including all contained subdirectories and files) mentioned in the file $script_dir/migrate_home_paths
 Lines in that file will be skipped if the line begins with a hash #.
 
 Arguments:
         --help  display this help and exit
 
+A logfile will be created at $script_dir/migrate_home.log.
 This script will overwrite any matching files and folders in the DEST directory.
-It will fail:
-    - completely if SOURCE directory doesnt exist
-    - on individual file if it exists at DEST and is of a different type (normal file or directory) than the one in SOURCE"
+It will fail on an individual file if it exists at DEST and is of a different type (normal file or directory) than the one in SOURCE.
+Known assumptions:
+    - That SOURCE, DEST and migrate_home_paths exist."
 
 #Display help if more than 2 args or 1 arg is --help
 if [ $# -eq 1 ] && [ $1 == "--help" ] || [ $# -ne 2 ]; then
@@ -37,9 +38,9 @@ main()  {
         #If everything is fine
         echo mig: attempting cp: "$REPLY"
         cp -av --parents "$REPLY" "$dest"
-    done < $input
+    done < "$script_dir"/migrate_home_paths
 
 }
 
 #Outputting stdout and stderr to logfile
-main 2>&1 | tee "$HOME/Scripts/migrate_home_log"
+main 2>&1 | tee "$script_dir"/migrate_home.log
