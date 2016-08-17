65c29bdb02d354e30988cd7b82a2e074306066ff
i-need-to-tell-you-something <toomassoomaa+github@gmail.com>
2016-08-17 21:49:32 +0300

    Complete rewrite. migrate_home.sh is now based on an external list of files and folders. Uses cp instead of unison for the backend. Make more safe. Add help message and lots of comments.

--- migrate_home.sh
+++ migrate_home.sh
@@ -1,42 +1,45 @@
 #!/bin/bash
 
-src=$1;
-dest=$2;
+src="$1";
+dest="$2";
+#the path of the file where the lines of filenames to be copied are
+input="$HOME/Scripts/migrate_home_paths"
+#The message that gets displayed as help
+help_message="Usage: migrate_home.sh SOURCE DEST
+Copy from SOURCE to DEST all the files and directories (including all contained subdirectories and files) mentioned in $input.
+Lines in that file will be skipped if the line begins with a hash #.
+
+Arguments:
+        --help  display this help and exit
+
+This script will overwrite any matching files and folders in the DEST directory.
+It will fail:
+    - completely if SOURCE directory doesnt exist
+    - on individual file if it exists at DEST and is of a different type (normal file or directory) than the one in SOURCE"
+
+#Display help if more than 2 args or 1 arg is --help
+if [ $# -eq 1 ] && [ $1 == "--help" ] || [ $# -ne 2 ]; then
+    echo "$help_message"
+    exit 1
+fi
 
 
-copy_single() {
-    [ -d $dest ] || mkdir $dest
-    [ -d $dest ] && mkdir -p $dest/$1
-    #mkdir $dest; echo $?
-    #mkdir $dest && echo success
-    #mkdir $dest/$1
-    unison $src/$1 $dest/$1 -batch -silent -dontchmod -perms 0 -force $src/$1
+main()  {
+    #If we cant change dir, then exit
+    cd "$src" || exit 1
+    while read -r; do
+        #Skip line if it begins with a hash.
+        [[ "$REPLY" =~ ^# ]] && echo mig: skipping "$REPLY" && continue
+        #Skip line if it doesnt begin with alphanumeric or punctuation.
+        [[ ! "$REPLY" =~ ^[[:graph:]] ]] && echo mig: malformed line: "$REPLY" && continue
+        #Skip line if $REPLY is missing in $src
+        [[ ! -e "$REPLY" ]] && echo mig: SOURCE missing for "$src"/"$REPLY" && continue
+        #If everything is fine
+        echo mig: attempting cp: "$REPLY"
+        cp -av --parents "$REPLY" "$dest"
+    done < $input
+
 }
 
-
-#copy_single .config/tox
-copy_single .config/qBittorrent
-#copy_single .eclipse
-copy_single .local/share/Aspyr/
-copy_single .minecraft
-#copy_single .mozilla
-#copy_single .PlayOnLinux
-#copy_single .Skype
-#copy_single .steam
-#copy_single .unison
-copy_single Desktop
-#copy_single Desktop 4
-copy_single Documents
-copy_single Downloads
-#copy_single eclipse 4.4.1
-#copy_single git
-#copy_single My Games
-#copy_single Scripts
-#copy_single Steam
-#copy_single workspace
-#copy_single .bash_history
-copy_single .face.icon
-copy_single .gitconfig
-copy_single kthx.kdbx
-copy_single n1.tar.gz.gpg
-copy_single n2.tar.gz.gpg
+#Outputting stdout and stderr to logfile
+main 2>&1 | tee "$HOME/Scripts/migrate_home_log"
