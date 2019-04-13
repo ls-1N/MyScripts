73163b81f0bd0cdb8bcb3be3df5d47760cb7dbc5
i-need-to-tell-you-something <toomassoomaa+github@gmail.com>
2019-04-13 15:50:34 +0300

    Fix filepath issue. Amend comments. Fix small log issue.

--- migrate_home.sh
+++ migrate_home.sh
@@ -18,7 +18,7 @@ help_message="Usage: migrate_home.sh SOURCE DEST
 Copy from SOURCE to DEST all the files and directories (including all contained subdirectories and files) mentioned in the file \$script_dir/migrate_home_paths
 Lines in that file will be skipped if the line begins with a hash #.
 
-Arguments:
+Arguments/Options:
         --help  display this help and exit
 
 A logfile will be created at \$script_dir/migrate_home.log.
@@ -26,7 +26,9 @@ This script will overwrite any matching files and folders in the DEST directory.
 It will fail on an individual file if it exists at DEST and is of a different type (normal file or directory) than the one in SOURCE.
 
 Known assumptions:
-    - That SOURCE, DEST and migrate_home_paths exist."
+    - That SOURCE, DEST and migrate_home_paths exist.
+    - Rsync 2.6.7 or newer.
+"
 
 # Display help if not exactly 2 args or if 1 arg is --help
 if [ $# -ne 2 ]; then
@@ -39,13 +41,15 @@ main()  {
     # If we cant change dir, then exit
     cd "$src" || exit 1
     while read -r; do
-        # Skip line if it begins with a hash.
+        # Skip a line if it begins with a hash.
         [[ "$REPLY" =~ ^# ]] && echo mig: skipping "$REPLY" && continue
+        # Skip a line if it is empty.
+        [[ -z "$REPLY" ]] && echo mig: skipping empty line && continue
         # Skip line if $REPLY is missing in $src
         [[ ! -e "$REPLY" ]] && echo mig: SOURCE missing for "$src"/"$REPLY" && continue
         # If everything is fine
-        echo mig: attempting rync: "$REPLY">> log_file 2>&1
-        rsync -avHAXEc --progress "$REPLY" "$dest" 2>&1 && echo mig: success rsync: "$REPLY" || echo mig: failure rsync: "$REPLY"
+        echo mig: attempting rync: "$REPLY"
+        rsync -aHAXEc -vR --progress "$REPLY" "$dest" 2>&1 && echo mig: success rsync: "$REPLY" || echo mig: failure rsync: "$REPLY"
     done < "$list_path"
 
 }
