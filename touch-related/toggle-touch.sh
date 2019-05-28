2aa584f3581e480f3e80a1666aa6a685d09130c9
i-need-to-tell-you-something <toomassoomaa+github@gmail.com>
2019-05-28 23:49:14 +0300

    Formatting changes to toggle-touch.sh.

--- touch-related/toggle-touch.sh
+++ touch-related/toggle-touch.sh
@@ -4,19 +4,20 @@
 ## for touch from `xsetwacom list dev`
 
 # Known problems:
-# Still outputs normal message when command fails
+# - Still outputs normal message when command fails
 
-DEVICE="Wacom HID 4846 Finger touch" 
+DEVICE="Wacom HID 4846 Finger touch"
+
 TOUCH_STATE=`xsetwacom get "$DEVICE" touch`
 MESSAGE_ON="Touch screen enabled"
 MESSAGE_OFF="Touch screen disabled"
 if [ "$TOUCH_STATE" == "on" ]
   then
-    xsetwacom set "$DEVICE" touch off && \
-    echo $MESSAGE_OFF && \
+    xsetwacom set "$DEVICE" touch off
+    echo $MESSAGE_OFF
     qdbus org.kde.plasmashell /org/kde/osdService org.kde.osdService.showText input-touchpad-off "$MESSAGE_OFF"
   else
-    xsetwacom set "$DEVICE" touch on && \
-    echo $MESSAGE_ON && \
+    xsetwacom set "$DEVICE" touch on
+    echo $MESSAGE_ON
     qdbus org.kde.plasmashell /org/kde/osdService org.kde.osdService.showText input-touchpad-on "$MESSAGE_ON"
 fi
