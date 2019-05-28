bfc9d59b51b362f23e32699f1a0a3c1dc4eb525d
i-need-to-tell-you-something <toomassoomaa+github@gmail.com>
2019-05-28 23:49:46 +0300

    Add a script to toggle screen-autorotate.

---
+++ touch-related/toggle-rotate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+# Known problems:
+# - Needs screenrotator installed
+# - Shows the unnecessary status icon
+
+MESSAGE_ON="Rotation lock on"
+MESSAGE_OFF="Rotation lock off"
+if [ `pgrep screenrotator` ]
+  then
+    killall screenrotator && \
+    echo $MESSAGE_ON && \
+    qdbus org.kde.plasmashell /org/kde/osdService org.kde.osdService.showText lock "$MESSAGE_ON"
+  else
+    screenrotator & \
+    echo $MESSAGE_OFF && \
+    qdbus org.kde.plasmashell /org/kde/osdService org.kde.osdService.showText unlock "$MESSAGE_OFF"
+fi
