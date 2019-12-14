22136d5cee476cc09ade24d391202a4d00b855c6
i-need-to-tell-you-something <toomassoomaa+github@gmail.com>
2019-12-15 00:04:27 +0200

    Remove the update. Add monitors. Run locally installed version.

--- pts_craftymon.sh
+++ pts_craftymon.sh
@@ -3,5 +3,4 @@
 
 #Run a specific benchmark after installing all prerequisites via apt.
 
-sudo apt-get update
-sudo apt-get install -y php-cli php-xml && sh /cdrom/phoronix-test-suite/phoronix-test-suite benchmark 1704241-KH-CRAFTYMON95
+sudo apt-get install -y php-cli php-xml && MONITOR=all phoronix-test-suite benchmark 1704241-KH-CRAFTYMON95
