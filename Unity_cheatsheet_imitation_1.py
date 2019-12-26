c6af4cef6c2967184c48467ee70ce5c8e8500026
i-need-to-tell-you-something <toomassoomaa+github@gmail.com>
2019-12-26 20:51:53 +0200

    Clean up white spaces. Change file paths.

--- Unity_cheatsheet_imitation_1.py
+++ Unity_cheatsheet_imitation_1.py
@@ -12,51 +12,51 @@ import sys
 from PyQt5.QtWidgets import QWidget, QDesktopWidget, QLabel, QApplication, QPlainTextEdit
 from PyQt5.QtCore import QFile, QIODevice, QTextStream, Qt
 
-file_path = './hotkey cheatsheet' #I think this is searched for from $HOME by default
+file_path = './hotkey cheatsheet'
 
 class Example(QWidget):
-    
+
     def __init__(self):
         super().__init__()
-        
+
         self.initUI()
         #self.setWindowFlags(Qt.FramelessWindowHint)
         #ui.setWindowFlags(Qt.Window | Qt.FramelessWindowHint)
-        
-        
-    def initUI(self):               
+
+
+    def initUI(self):
 
         font1 = self.font()
         font1.setPointSize(QDesktopWidget().availableGeometry().height()/60)
         self.setFont(font1)
-        
+
         text=open(file_path).read()
         lbl1 = QLabel(text, self)
-        
-        self.setWindowTitle('Hotkeys')    
+
+        self.setWindowTitle('Hotkeys')
         self.show()
         self.adjustSize()
         hsize = (QDesktopWidget().availableGeometry().width() / 6 * 5)
         self.setMinimumWidth(hsize)
         self.center()
         self.setWindowOpacity(0.95)
-        
-        
+
+
     def center(self):
-        
+
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
-        
+
     #def focusOutEvent(self, event):
-        
+
         #print ("trying")
         #sys.exit()
-        
-        
+
+
 if __name__ == '__main__':
-    
+
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
