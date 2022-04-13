68864ae0b6a41b550e3b5d32b4a73f77d2ec249f
i-need-to-tell-you-something <toomassoomaa+github@gmail.com>
2022-04-14 00:58:07 +0300

    Add BT hardware volume adjustment script

---
+++ bt_volume.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from pulsectl import Pulse
+import subprocess
+import sys
+
+# Prerequisite tasks on ubuntu machine:
+# sudo apt install python3-pip
+# pip install pulsectl
+
+# diff applies to non-BT Pulse devices
+#diff = int(sys.argv[1])
+method = 'org.bluez.MediaControl1.Volume{}'.format(
+  'Down' if sys.argv[1] == 'down' else 'Up')
+
+with Pulse() as pulse:
+  for sink in pulse.sink_list():
+    bluez_path = sink.proplist.get('bluez.path')
+    if bluez_path:
+        args = [
+            'dbus-send', '--system', '--print-reply',
+            '--dest=org.bluez', bluez_path, method,
+        ]
+        subprocess.run(args, check=True)
+    #else:
+        #pulse.volume_change_all_chans(sink, diff)
