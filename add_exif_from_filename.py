ec79043e22b3892d5f9c5dd7426027d1239d13f4
ls-1N <9165395+ls-1N@users.noreply.github.com>
2024-07-01 02:33:01 +0300

    Improve strings

--- add_exif_from_filename.py
+++ add_exif_from_filename.py
@@ -10,14 +10,14 @@ import re
 import subprocess
 import sys
 
-pattern = re.compile('\d\d\d\d\d\d\d\d_\d\d\d\d\d\d')
+pattern = re.compile(r'\d{8}_\d{6}')
 
 rootdir = Path(sys.argv[1])
 for file in rootdir.iterdir():
     if file.is_file():
         date_match = re.search(pattern, str(file))
         dt = date_match.group(0)
-        dt = dt[:4] + ':' + dt[4:6] + ':' + dt[6:8] + ' ' + dt[9:11] + ':' + dt[11:13] + ':' + dt[13:15]
+        dt = f'{dt[:4]}:{dt[4:6]}:{dt[6:8]} {dt[9:11]}:{dt[11:13]}:{dt[13:15]}'
         command = f'exiftool -modifydate="{dt}" -createdate="{dt}" {file}'
         print(command)
         subprocess.run(command, shell=True, stdout=subprocess.PIPE)
